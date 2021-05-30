b08e224b9cb2a9d75070ec9d2a195fe30e41b204
DmitrySimakov <83167036+DmitrySimakov@users.noreply.github.com>
2021-05-30 15:16:30 +0500

    Add files via upload

---
+++ lesson_6_homework.sql
@@ -0,0 +1,24 @@
+--схема БД: https://docs.google.com/document/d/1NVORWgdwlKepKq_b8SPRaSpraltxoMg2SIusTEN6mEQ/edit?usp=sharing
+--colab/jupyter: https://colab.research.google.com/drive/1j4XdGIU__NYPVpv74vQa9HUOAkxsgUez?usp=sharing
+
+--task1  (lesson6, дополнительно)
+-- SQL: Создайте таблицу с синтетическими данными (10000 строк, 3 колонки, все типы int) и заполните ее случайными данными от 0 до 1 000 000. Проведите EXPLAIN операции и сравните базовые операции.
+
+create table random as 
+select generate_series(1, 10000) as id
+, cast (random() * 1000000 as int) as r1
+, cast (random() * 1000000 as int) as r2
+, cast (random() * 1000000 as int) as r3
+
+explain 
+select *
+from random 
+where r1 > 9999
+
+explain analyze 
+select *
+from random 
+where r1 > 9999
+
+--task2 (lesson6, дополнительно)
+-- GCP (Google Cloud Platform): Через GCP загрузите данные csv в базу PSQL по личным реквизитам (используя только bash и интерфейс bash) 
